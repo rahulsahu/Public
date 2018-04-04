d59fd0e927c07789c559bf472177d02f807bb4a4
Rahul.Sahu <sahu.rahul10@gmail.com>
2018-04-04 11:47:55 +0530

    test

--- Cache/src/main/java/com/avirah/cache/Cache.java
+++ Cache/src/main/java/com/avirah/cache/Cache.java
@@ -1,11 +1,20 @@
 package com.avirah.cache;
 
+import java.util.concurrent.TimeUnit;
+
 public class Cache<K extends Object, V extends Object> {
 
 	private CacheLoader<K, V> cacheLoader;
+	@SuppressWarnings("unused")
+	private TimeUnit timeUnit;
+	@SuppressWarnings("unused")
+	private int expireAfter;
 
-	private Cache(CacheLoader<K, V> cacheLoader) {
-		this.cacheLoader = cacheLoader;
+	@SuppressWarnings("unchecked")
+	private Cache(CacheBuilder builder) {
+		cacheLoader = builder.cacheLoader;
+		expireAfter = builder.expireAfter;
+		timeUnit = builder.timeUnit;
 	}
 
 	public V get(K k) {
@@ -16,7 +25,15 @@ public class Cache<K extends Object, V extends Object> {
 		cacheLoader.refresh();
 	}
 
+	
+	
 	public static class CacheBuilder {
+		
+		@SuppressWarnings("rawtypes")
+		private CacheLoader cacheLoader;
+		private TimeUnit timeUnit;
+		private int expireAfter;
+		
 		private CacheBuilder() {
 
 		}
@@ -25,9 +42,23 @@ public class Cache<K extends Object, V extends Object> {
 			return new CacheBuilder();
 
 		}
+		
+		public CacheBuilder expireAfter(int expireAfter,TimeUnit timeUnit) {
+			this.expireAfter = expireAfter;
+			this.timeUnit = timeUnit;
+			return this;
+			
+		}
 
-		public <K, V> Cache<K, V> build(CacheLoader<K, V> cacheLoader) {
-			return new Cache<K, V>(cacheLoader);
+		public <K, V> CacheBuilder loader(CacheLoader<K, V> cacheLoader) {
+			
+			this.cacheLoader = cacheLoader;
+			return this;
+			
+		}
+		
+		public <K, V> Cache<K, V> build() {
+			return new Cache<K, V>(this);
 		}
 	}
 
--- Cache/src/main/java/com/avirah/demo/AppTest.java
+++ Cache/src/main/java/com/avirah/demo/AppTest.java
@@ -1,5 +1,7 @@
 package com.avirah.demo;
 
+import java.util.concurrent.TimeUnit;
+
 import com.avirah.cache.Cache;
 import com.avirah.cache.CacheLoader;
 import com.avirah.cache.Cache.CacheBuilder;
@@ -9,15 +11,14 @@ public class AppTest {
 	private static Cache<Long, String> cache;
 
 	static {
-		
-		cache  = CacheBuilder.newBuilder().build(new CacheLoader<Long, String>() {
+		cache  = CacheBuilder.newBuilder().loader(new CacheLoader<Long, String>() {
 			@Override
 			protected String load(Long k) {
 				// write your own loading mechanism
 				String data = k + " - > Data";
 				return data;
 			}
-		});
+		}).expireAfter(10, TimeUnit.MINUTES).build();
 	}
 
 	public static void main(String[] args) throws InterruptedException {
